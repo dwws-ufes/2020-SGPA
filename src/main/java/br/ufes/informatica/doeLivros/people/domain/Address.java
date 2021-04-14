6cfec08e5a5f0709a42966e2352349e712b7d954
Bruno <Bruno@DESKTOP-QDMTE0L>
2021-04-13 22:01:34 -0300

    Atualizações:
    - Implementada funcionalidade de login;
    - Reorganização de Pastas;

--- src/main/java/br/ufes/informatica/doeLivros/people/domain/Address.java
+++ src/main/java/br/ufes/informatica/doeLivros/people/domain/Address.java
@@ -2,7 +2,6 @@ package br.ufes.informatica.doeLivros.people.domain;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToOne;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import br.ufes.inf.nemo.jbutler.ejb.persistence.PersistentObjectSupport;
